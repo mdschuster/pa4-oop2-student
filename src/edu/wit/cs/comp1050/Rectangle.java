f361b2186a2d75a41324543e7bd353ccef7cf706
Charlie Wiseman <wisemanc@wit.edu>
2017-10-12 22:16:31 -0400

    Updating notes and hints.

--- src/edu/wit/cs/comp1050/Rectangle.java
+++ src/edu/wit/cs/comp1050/Rectangle.java
@@ -2,7 +2,9 @@ package edu.wit.cs.comp1050;
 
 //TODO: document this class
 public class Rectangle extends Shape2D {
-	
+    // think about how you want to store the vertices of this rectangle
+    // hint: an array might make sense
+    
 	/**
 	 * Constructs a rectangle given two points
 	 * 
@@ -12,6 +14,7 @@ public class Rectangle extends Shape2D {
 	 */
 	public Rectangle(String color, Point2D p1, Point2D p2) {
 		super(color, ""); // replace with your code
+		// note: you can't assume that the points p1 and p2 are given to you in any particular order
 	}
 	
 	/**
@@ -47,6 +50,8 @@ public class Rectangle extends Shape2D {
 		return null; // replace with your code
 	}
 
+    // hint: it may help to add a few private methods for distance between the two corners
+    
 	@Override
 	public double getArea() {
 		return 0; // replace with your code
